343d9cc6490888197bb9ab1fef471a7e4f86b48c
Jeff Turner <Jeffrey.L.Turner@me.com>
2016-12-29 13:14:33 -0800

    bd does not work reliable

--- posh-git.ps1
+++ posh-git.ps1
@@ -10,7 +10,7 @@ Import-Module posh-git
 [System.Collections.Stack]$GLOBAL:dirStack = @()
 $global:LASTEXITCODE = $realLASTEXITCODE
 $GLOBAL:oldDir = '~'
-$GLOBAL:nowPath = ''
+$GLOBAL:nowPath = '~'
 $GLOBAL:addToStack = $true
 
 # Set up a simple prompt, adding the git prompt parts inside git repos, enable bd/back directory function
@@ -19,7 +19,13 @@ function global:prompt {
     $global:LASTEXITCODE = $realLASTEXITCODE
 
     $GLOBAL:nowPath = (Get-Location).Path
-    if(($GLOBAL:nowPath -ne $oldDir) -AND ($GLOBAL:addToStack)){
+    if(($GLOBAL:nowPath -ne $GLOBAL:oldDir) -AND ($GLOBAL:addToStack)){
+    	Write-Host(" nowPAth changed: ") -nonewline
+    	Write-Host($GLOBAL:nowPath) 
+
+    	if(($GLOBAL:dirStack.count) -gt 1){
+            $GLOBAL:dirStack = @()
+    	}
         $GLOBAL:dirStack.Push($GLOBAL:oldDir)
         $GLOBAL:oldDir = $GLOBAL:nowPath
     }
@@ -35,14 +41,16 @@ function global:prompt {
 function BackOneDir{
     if(($GLOBAL:dirStack.count) -gt 1){
         $GLOBAL:oldDir = $GLOBAL:dirStack.Pop()
+        $GLOBAL:dirStack = @()
+        $GLOBAL:dirStack.Push($GLOBAL:nowPath)
     } else {
         $GLOBAL:oldDir = $GLOBAL:dirStack.Pop()
         $GLOBAL:dirStack.Push($GLOBAL:nowPath)
     }
     $GLOBAL:addToStack = $false
     cd $GLOBAL:oldDir
-
 }
+
 Set-Alias bd BackOneDir
 
 Pop-Location
